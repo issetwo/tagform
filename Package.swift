accb05dc40c5d5787283fa7b3a67c22666d90c99
Kazuto Yamada <issetwo@gmail.com>
2022-05-12 01:04:49 +0900

    Test用のコード削除

--- Package.swift
+++ Package.swift
@@ -21,8 +21,5 @@ let package = Package(
         .target(
             name: "TagEditField",
             dependencies: []),
-        .testTarget(
-            name: "TagEditFieldTests",
-            dependencies: ["TagEditField"]),
     ]
 )
